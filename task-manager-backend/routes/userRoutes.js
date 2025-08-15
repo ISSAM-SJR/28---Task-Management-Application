c3f1318d2b3a08f0a7e7bfa7f11942a95bf84fa4
ISSAM-SJR <emna.amarez@gmail.com>
2025-08-15 18:45:51 +0100

    second commit

---
+++ task-manager-backend/routes/userRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+require('dotenv').config({ path: '..env' });
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({ username, password });
+  await user.save();
+  res.status(201).json({ message: 'User registered' });
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+});
+
+module.exports = router;
